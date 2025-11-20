c081ac91192929723cf2fc8ab742e1002f25531b
adam.bohannon <adam.bohannon@housecallpro.com>
2025-11-20 08:14:55 -0800

    forget-me-not

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,3 +3,8 @@
 </svelte:head>
 
 <a href="https://shsdavisapes.pbworks.com/f/Omelas.pdf" class="text-white text-6xl no-underline hover:underline">.</a>
+
+<a
+	href="https://www.orwellfoundation.com/the-orwell-foundation/orwell/essays-and-other-works/politics-and-the-english-language/"
+	class="fixed bottom-4 right-4 text-white text-6xl no-underline hover:underline"
+>_</a>
